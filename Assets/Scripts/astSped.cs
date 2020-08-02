6f5430ed24821b94fbe864ea53476bf499f3fee3
Daniel Zhuang <danielzhuang11@hotmail.com>
2020-08-02 16:35:49 -0500

    added enemy ship

--- Assets/Scripts/astSped.cs
+++ Assets/Scripts/astSped.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class astSped : MonoBehaviour
 {
+   
 
     // Update is called once per frame
     void Update()
     {
         if (globalScore.coins % 3 == 0) {
-            GetComponent<Rigidbody2D>().gravityScale = 1 + globalScore.coins / 3;
-                }
+             GetComponent<Rigidbody2D>().gravityScale = 1 + globalScore.coins / 3; 
+
+        }
+
     }
+  
 }
